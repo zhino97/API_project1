44e6d1b40b5e70d772486e167833509af3b47c37
zhino <zhinosabahh@yahoo.com>
2021-11-10 00:48:59 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Quote from "./component/Quote";
+import "./App.css";
+import "./component/Qoute.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Quote />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Quote.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import "./Qoute.css";
+import axios from "axios";
+
+const App = () => {
+  const [qoute, setQoute] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const qouteAPI = async () => {
+    let arrayOfQoutes = [];
+    try {
+      const data = await axios.get("http://api.quotable.io/random");
+      arrayOfQoutes = data.data;
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      setQoute(arrayOfQoutes.content);
+      setAuthor(arrayOfQoutes.author);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    qouteAPI();
+  }, []);
+
+  return (
+    <div>
+      <div className="qouteBox">
+        <div className="container">
+          <div>
+            <button onClick={qouteAPI} className="btn btn-danger">
+              CHANGE CUOTE
+            </button>
+          </div>
+          <div className="qoute">
+            <h2>{qoute}</h2>
+          </div>
+          <div className="author">{author}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
